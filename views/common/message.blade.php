68876f02829fa434500259fe7fc4ada87a1615e2
skys215 <skys215@gmail.com>
2022-09-17 16:15:56 +0800

    Fixed bug, alert with level

--- views/common/message.blade.php
+++ views/common/message.blade.php
@@ -1,6 +1,6 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
-    <div class="alert alert-success">
-        <i class="bi bi-check-circle"></i> {!! $message['message'] !!}
+    <div class="alert alert-{{ $message['level'] }}">
+        {!! $message['message'] !!}
     </div>
 @endforeach
 {{ session()->forget('flash_notification') }}
