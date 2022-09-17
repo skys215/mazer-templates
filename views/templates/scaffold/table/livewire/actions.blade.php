f230c1de96525cd7f19b7348aa0e1072f10f836d
skys215 <skys215@gmail.com>
2022-09-17 14:32:38 +0800

    Removed lang from livewire table, not supported

--- views/templates/scaffold/table/livewire/actions.blade.php
+++ views/templates/scaffold/table/livewire/actions.blade.php
@@ -1,13 +1,13 @@
 <div class="btn-group">
     <a href="{{ $showUrl }}" class='btn btn-sm btn-primary'>
-        @if($config->options->localized) @@lang('crud.detail') @else View @endif
+        View
     </a>
     <a href="{{ $editUrl }}" class='btn btn-sm btn-success'>
-        @if($config->options->localized) @@lang('crud.edit') @else Edit @endif
+        Edit
     </a>
     <a href="#!" wire:click="deleteRecord({{ $recordId }})"
-        onclick="confirm(@@lang('crud.are_you_sure') @else 'Are you sure?' @endif) || event.stopImmediatePropagation()"
+        onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
         class='btn btn-sm btn-danger'>
-        @if($config->options->localized) @@lang('crud.delete') @else Delete @endif
+        Delete
     </a>
 </div>
